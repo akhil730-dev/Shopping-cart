a0977dc3ff1ff9f7aa5ccd7b35a762946e5974e8
Akhil <akhilofficial9303@gmail.com>
2026-03-20 15:45:51 +0530

    Update CORS for Railway backend

--- Backend/server.js
+++ Backend/server.js
@@ -21,7 +21,7 @@ await connectDB()
 
 
 //Alow Multiple Origins
-const allowedOrigins=["http://localhost:5173",'https://shopping-cart-one-snowy.vercel.app']
+const allowedOrigins=["http://localhost:5173",'https://shopping-cart-one-snowy.vercel.app',]
 
 /// Middleware configuration 
 
